0678309438ca557dc82a62bd3028ca256f6fad19
master <tuco.anderson@gmail.com>
2020-01-16 20:43:41 -0300

    backup pasta web

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
+
+function App() {
+  const [devs, setDevs] = useState([]);
+
+  
+
+  
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, [])
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
+    setDevs([...devs, response.data]);
+  }
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
+
+// componente: bloco isolado de html, js, css o qual não interfere no restante da aplicação
+//propriedade: informações que um componente pai passa para o componente filho
+//estado: informações mantidas pelo componente (lembrar imutabilidade)
